9a3fcdd0a7279b8984996ed829277565c10890cc
tlarbals824 <simkm0824@naver.com>
2023-06-19 20:28:56 +0900

    [chore] 환경 설정

---
+++ src/test/java/com/sim/wellog/WellogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sim.wellog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
